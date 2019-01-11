896c3b38b89c209c3a17864ff5cbd04689138a16
chenguanglyc <920442875@qq.com>
2019-01-11 21:05:15 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,18 @@
+package com.atguigu.bean;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Customer {
+    @Override
+    public String toString() {
+        System.out.print("ssqq");
+
+        System.out.print("sss");
+
+        Date date = new Date();
+        ArrayList list = new ArrayList();
+        return super.toString();
+
+    }
+}
